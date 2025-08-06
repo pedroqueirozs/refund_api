d954601b09f8e083b700ec403a4f34f6ebf1f70f
Pedro Queiroz <douglas.333.goncalves@gmail.com>
2025-08-05 21:00:02 -0300

    applying try catch in sessions-controller.ts

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { prisma } from "@/database/prisma";
 import { AppError } from "@/utils/AppError";
 import z, { object } from "zod";
@@ -7,42 +7,46 @@ import { authConfig } from "@/configs/auth";
 import { sign } from "jsonwebtoken";
 
 class SessionsController {
-  async create(request: Request, response: Response) {
-    const bodySchema = object({
-      email: z.string().email({ message: "E-mail inválido" }),
-      password: z.string(),
-    });
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = object({
+        email: z.string().email({ message: "E-mail inválido" }),
+        password: z.string(),
+      });
 
-    const { email, password } = bodySchema.parse(request.body);
+      const { email, password } = bodySchema.parse(request.body);
 
-    const user = await prisma.user.findFirst({
-      where: { email },
-    });
+      const user = await prisma.user.findFirst({
+        where: { email },
+      });
 
-    if (!user) {
-      throw new AppError("E-mail ou senha inválido", 401);
-    }
-    const passwordMatched = await compare(password, user.password);
-
-    if (!passwordMatched) {
-      throw new AppError("E-mail ou senha inválido", 401);
-    }
-
-    const { secret, expiresIn } = authConfig.jwt;
-
-    const token = sign(
-      {
-        role: user.role,
-      },
-      secret,
-      {
-        subject: user.id,
-        expiresIn,
+      if (!user) {
+        throw new AppError("E-mail ou senha inválido", 401);
       }
-    );
+      const passwordMatched = await compare(password, user.password);
 
-    const { password: _, ...userWithoutPassword } = user;
-    response.json({ token, user: userWithoutPassword });
+      if (!passwordMatched) {
+        throw new AppError("E-mail ou senha inválido", 401);
+      }
+
+      const { secret, expiresIn } = authConfig.jwt;
+
+      const token = sign(
+        {
+          role: user.role,
+        },
+        secret,
+        {
+          subject: user.id,
+          expiresIn,
+        }
+      );
+
+      const { password: _, ...userWithoutPassword } = user;
+      response.json({ token, user: userWithoutPassword });
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
